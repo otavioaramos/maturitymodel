de650d2231f77236bcd8fb4f867279e3b06cfb1a
Otávio Ramos <otavio.ramos@viavarejo.com.br>
2024-06-28 10:30:07 -0300

    commit inicial

---
+++ src/main/java/br/com/maturitymodel/maturitymodel/MaturitymodelApplication.java
@@ -0,0 +1,13 @@
+package br.com.maturitymodel.maturitymodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaturitymodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaturitymodelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/maturitymodel/maturitymodel/MaturitymodelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maturitymodel.maturitymodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaturitymodelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
