a140c555db531e6956018e8fda8922d24ac4be74
Otávio Ramos <otavio.ramos@viavarejo.com.br>
2025-01-28 14:32:38 -0300

    alteracoes

---
+++ src/main/java/br/com/maturitymodel/maturitymodel/config/Seguranca.java
@@ -0,0 +1,2 @@
+package br.com.maturitymodel.maturitymodel.config;public class Seguranca {
+}
---
+++ src/test/java/br/com/maturitymodel/maturitymodel/controller/AplicacoesControllerTest.java
@@ -0,0 +1,2 @@
+package br.com.maturitymodel.maturitymodel.controller;public class AplicacoesControllerTest {
+}
